c8dd4ab5f750f379ab3d5dc726b0fe1efda95537
ChengCheng <athecheng1010@gmail.com>
2021-08-25 21:48:41 -0700

    first commit

---
+++ app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,9 @@
+package com.codinginflow.imagesearchapp.ui.gallery
+
+import androidx.fragment.app.Fragment
+import com.codinginflow.imagesearchapp.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+}
